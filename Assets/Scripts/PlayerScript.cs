52c6bc49ce537e7b8fac02a35ad1774839b34423
thetrav <the.trav@gmail.com>
2014-02-03 07:14:13 +1100

    scene with multi cam, space ship control and rotation

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 accel = new Vector2(1, 1);
+	private Vector2 movement;
+	private float bearing = 180;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector2(
+			accel.x * inputX,
+			accel.y * inputY);
+
+		Vector2 velocity = rigidbody2D.velocity;
+
+		float mag = velocity.magnitude;
+
+		if (mag > 0) {
+			float angle = Mathf.Atan2 (velocity.x, velocity.y);
+			bearing = 180 - (57.295f * angle);
+		}
+	}
+
+	void FixedUpdate() {
+		rigidbody2D.AddForce(movement);
+		transform.localEulerAngles = new Vector3 (0, 0, (int)bearing);
+	}
+}
