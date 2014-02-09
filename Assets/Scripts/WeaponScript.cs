c9c0be87370c43cbf2d9b538326698d025f5e021
thetrav <the.trav@gmail.com>
2014-02-09 14:50:38 +1100

    add assets from Long, implement weapon mount and controls

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public float rotation = 0.0f;
+	public float aimSpeed = 0.5f;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("[")) {
+				rotation = aimSpeed;
+		} else if (Input.GetKey ("]")) {
+				rotation = aimSpeed * -1.0f;
+		} else {
+				rotation = 0.0f;
+		}
+	}
+
+	void FixedUpdate() {
+		transform.Rotate (new Vector3 (0, 0, 1), rotation);
+	}
+}
